b49728811b872e2e48a6f0921f0437dd9c3e6bd1
Alethia Quintero <alethiaq22@gmail.com>
2019-08-01 21:51:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,11 @@ class Scraper
       end
     end
   end
-  
+
+
+  def get_page 
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
 end
 
 
